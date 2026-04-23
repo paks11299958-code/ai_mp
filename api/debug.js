ee0b1adc4a75a9328836524f2266a8c90a0fcd77
paks11299958-code <paks11299958@gmail.com>
2026-04-23 14:19:08 +0900

    Delete api/debug.js

--- api/debug.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  res.json({
-    DATABASE_URL: process.env.DATABASE_URL ? 'SET' : 'NOT SET',
-    JWT_SECRET: process.env.JWT_SECRET ? 'SET' : 'NOT SET',
-    GOOGLE_CLOUD_PROJECT: process.env.GOOGLE_CLOUD_PROJECT ? 'SET' : 'NOT SET',
-    PROXY_HEADER: process.env.PROXY_HEADER ? 'SET' : 'NOT SET',
-    NODE_ENV: process.env.NODE_ENV,
-  });
-};
