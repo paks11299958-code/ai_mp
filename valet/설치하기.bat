9a3eca44afa39939fabc484269c8c36324c693ef
paks11299958 <paks1012@naver.com>
2026-05-21 14:35:41 +0000

    fix: 설치하기.bat에 playwright 본체 추가

--- valet/설치하기.bat
+++ valet/설치하기.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0"
-npm install playwright-extra puppeteer-extra-plugin-stealth
+npm install playwright playwright-extra puppeteer-extra-plugin-stealth
 call npx playwright install chromium
 pause
